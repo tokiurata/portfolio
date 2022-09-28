2f31a40832b8ff380f9f3602118588762b6b6e27
Toki Urata <tokiurata@outlook.com>
2022-09-28 17:45:11 -0400

    Create Hero component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,8 +1,16 @@
 import "./Hero.scss";
+import avatar from "../../assets/avatar.webp";
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div className="hero">
+      <div className="hero__copy-container">
+        <h2 className="hero__copy">
+          Hello! My name is Toki. I'm a <br/><span className="hero__copy-big">Front End Developer</span><br/>currently focusing on <span className="underline">React</span>, <span className="underline">Redux</span>, and <span className="underline">TypeScript</span>.
+        </h2>
+      </div>
+      <img className="avatar" src={avatar} alt="Toki Urata"></img>
+    </div>
   );
 }
 
