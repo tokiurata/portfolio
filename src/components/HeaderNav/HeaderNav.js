11acf2101231cb749d25d79f0a4fca20e92fab15
Toki Urata <tokiurata@outlook.com>
2022-09-29 15:22:12 -0400

    Update styles

--- src/components/HeaderNav/HeaderNav.js
+++ src/components/HeaderNav/HeaderNav.js
@@ -3,11 +3,12 @@ import "./HeaderNav.scss";
 function HeaderNav() {
   return (
     <nav className="headernav">
-      <h1 className="logo">Toki Urata</h1>
+      <h1 className="logo-name">Toki Urata</h1>
       <ul className="headernav__nav">
-        <li>Projects</li>
-        <li>Tech Stack</li>
-        <li>Contact</li>
+        <li className="headernav__nav-item">Home</li>
+        <li className="headernav__nav-item">Tech Stack</li>
+        <li className="headernav__nav-item">Projects</li>
+        <li className="headernav__nav-item">Contact</li>
       </ul>
     </nav>
   );
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,15 +1,24 @@
 import "./Hero.scss";
-import avatar from "../../assets/avatar.webp";
+// import avatar from "../../assets/avatar.webp";
 
 function Hero() {
   return (
     <div className="hero">
-      <div className="hero__copy-container">
-        <h2 className="hero__copy">
-          Hello! My name is Toki. I'm a <br/><span className="hero__copy-big">Front End Developer</span><br/>currently focusing on <span className="underline">React</span>, <span className="underline">Redux</span>, and <span className="underline">TypeScript</span>.
+      <div className="hero__copy">
+        <h2 className="hero__header">
+          Hi! My name is Toki. I'm a 
         </h2>
+        <h2 className="hero__header hero__header--big">
+          Front End Developer
+        </h2>
+        <h2 className="hero__header">
+          based in Canada. 🇨🇦
+        </h2>
+        <p className="hero__body">
+          I'm a lifelong learner, attentive to detail, and a hardworking person. I strongly believe in teamwork and I love helping others. I'm currently focusing on <span className="hero__body--span">React</span>, <span className="hero__body--span">Redux</span>, and <span className="hero__body--span">TypeScript</span>.
+        </p>
       </div>
-      <img className="avatar" src={avatar} alt="Toki Urata"></img>
+      {/* <img className="avatar" src={avatar} alt="Toki Urata"></img> */}
     </div>
   );
 }
