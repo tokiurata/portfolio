c96f17b7a15a45fb9ae0b19fcade0f890ea22c12
Toki Urata <tokiurata@outlook.com>
2022-09-30 12:05:06 -0400

    Create Footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from "./components/Hero/Hero";
 import Projects from "./components/Projects/Projects";
 import TechStack from "./components/TechStack/TechStack";
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer"
 
 import "./App.scss";
 
@@ -14,6 +15,7 @@ function App() {
       <Projects />
       <TechStack />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <small className="footer__author"><span className="footer__copyright">©</span> Toki Urata</small>
+    </div>
+  );
+}
+
+export default Footer;
