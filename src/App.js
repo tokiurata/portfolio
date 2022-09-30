35798c5c54319c53ca0b353bf877a083f5171a97
Toki Urata <tokiurata@outlook.com>
2022-09-29 22:56:07 -0400

    Create Projects component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import HeaderNav from "./components/HeaderNav/HeaderNav";
 import Hero from "./components/Hero/Hero";
+import Projects from "./components/Projects/Projects"
 
 import "./App.scss";
 
@@ -8,6 +9,7 @@ function App() {
     <div className="app">
       <HeaderNav />
       <Hero />
+      <Projects />
     </div>
   );
 }
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,30 @@
+import "./Projects.scss";
+
 function Projects() {
   return (
-    <div>
-      Projects
+    <div className="projects" id="projects">
+      <div className="project">
+        <div className="project__copy">
+          <h3 className="project__header">Project Title</h3>
+          <p className="project__body">
+            Description of the project. Blah blah blah HTML, CSS, Sass, JavaScript
+          </p>
+        </div>
+        <div className="project__image-container">
+          -- here goes the image of the project --
+        </div>
+      </div>
+      <div className="project">
+        <div className="project__image-container">
+          -- here goes the image of the project --
+        </div>
+        <div className="project__copy">
+          <h3 className="project__header">Project Title</h3>
+          <p className="project__body">
+            Description of the project. Blah blah blah HTML, CSS, Sass, JavaScript
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
