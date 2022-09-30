bb67094697881c1c05b07040301bb94215842e2d
Toki Urata <tokiurata@outlook.com>
2022-09-30 11:46:28 -0400

    Create Contact component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import HeaderNav from "./components/HeaderNav/HeaderNav";
 import Hero from "./components/Hero/Hero";
 import Projects from "./components/Projects/Projects";
 import TechStack from "./components/TechStack/TechStack";
+import Contact from "./components/Contact/Contact";
 
 import "./App.scss";
 
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <Projects />
       <TechStack />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,26 @@
+import avatar from "../../assets/images/avatar.webp";
+import githubIcon from "../../assets/icons/github-light.svg";
+ 
+import "./Contact.scss";
+
+function Contact() {
+  return (
+    <div className="contact" id="contact">
+      <h3 className="contact__title">Contact</h3>
+      <div className="contact__content">
+        <img className="contact__image" src={avatar} alt="Toki Urata" />
+        <address className="contact__copy">
+          <h4 className="contact__header">Email</h4>
+          <a className="contact__body" href="mailto:tokiurata@outlook.com">tokiurata@outlook.com</a>
+          <h4 className="contact__header">GitHub</h4>
+          <a className="contact__body" href="https://github.com/TokiUrata">
+            <img src={githubIcon} className="contact__icon" alt="GitHub" />
+          </a>
+
+        </address>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
